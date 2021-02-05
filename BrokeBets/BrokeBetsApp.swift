16643ab290fea6aec4ac154909efd9c4ff33ddbc
Todd Weidler <rokstarrtodd@hotmail.com>
2021-02-05 14:29:59 -0500

    Initial Commit

---
+++ BrokeBets/BrokeBetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrokeBetsApp.swift
+//  BrokeBets
+//
+//  Created by Todd Weidler on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct BrokeBetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
