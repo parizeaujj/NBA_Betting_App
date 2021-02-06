0458b285fa59a8dbeee3015b4ba4b318a4a5738a
Aland Nguyen <nguyenaland@yahoo.com>
2021-02-06 17:07:30 -0500

    Aland's Test

--- BrokeBets/ContentView.swift
+++ BrokeBets/ContentView.swift
@@ -14,20 +14,27 @@ struct ContentView: View {
         ZStack {
             
             Color(red: 240/255, green: 243/255, blue: 248/255, opacity: 1)
-
             VStack {
-                Text("Broke Bets")
-                    .font(.largeTitle)
-                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(Color(red: 43/255, green: 43/255, blue: 43/255, opacity: 1))
+                
+                Text("Hello World")
+                VStack {
+                    Text("Broke Bets")
+                        .font(.largeTitle)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(Color(red: 43/255, green: 43/255, blue: 43/255, opacity: 1))
+                }
+                .padding(20)
+                .background(Color(red: 254/255, green: 255/255, blue: 255/255, opacity: 1))
+                .cornerRadius(5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color(red: 210/255, green: 215/255, blue: 222/255, opacity: 1), lineWidth: 1)
+                )
+                
             }
-            .padding(20)
-            .background(Color(red: 254/255, green: 255/255, blue: 255/255, opacity: 1))
-            .cornerRadius(5)
-            .overlay(
-                RoundedRectangle(cornerRadius: 5)
-                    .stroke(Color(red: 210/255, green: 215/255, blue: 222/255, opacity: 1), lineWidth: 1)
-            )
+            
+            
+            
         }.edgesIgnoringSafeArea(.all)
     }
 }
