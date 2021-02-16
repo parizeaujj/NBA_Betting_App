e89c2fbaaec9786565f3153fc8850442e714f2d2
Todd Weidler <rokstarrtodd@hotmail.com>
2021-02-15 19:41:16 -0500

    added pods

---
+++ Pods/Target Support Files/Pods-BrokeBets-BrokeBetsUITests/Pods-BrokeBets-BrokeBetsUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_BrokeBets_BrokeBetsUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BrokeBets_BrokeBetsUITestsVersionString[];
+
