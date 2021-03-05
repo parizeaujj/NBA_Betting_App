12022e9a2dadd28a82e128782b7a6b8216531e36
Fabrizio Herrera <fabrizioherrera99@hotmail.com>
2021-03-04 23:17:38 -0500

    Search bar preview

---
+++ BrokeBets/SearchBar.swift
@@ -0,0 +1,34 @@
+//
+//  SearchBar.swift
+//  BrokeBets
+//
+//  Created by Fabrizio Herrera on 3/4/21.
+//
+
+import Foundation
+import SwiftUI
+import Firebase
+
+struct SearchBarView: View {
+    
+    var body: some View {
+        
+        ZStack(alignment: .top) {
+            
+            GeometryReader {_ in
+                
+                
+            }.background(Color("Color"))
+            .edgesIgnoringSafeArea(.all)
+            
+        }
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        SearchBarView()
+    }
+}
